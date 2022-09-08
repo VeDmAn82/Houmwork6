3580481c90fbbbf40a63850f0306d30d29de63d4
Dmitry Vechkhayzer <dmitry.v82@mail.ru>
2022-09-08 17:22:24 +0400

    add decision Task41

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -44,7 +44,7 @@ void PrintArray(int[]array)
     Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write($"{array[i]} ");       
+        Console.Write($"{array[i]}, ");       
     }
     Console.Write("]");
 }
